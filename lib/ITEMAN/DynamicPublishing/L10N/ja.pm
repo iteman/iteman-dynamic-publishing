a03aeb6d5b5b5b795618f60688b03af52f65cae2
KUBO Atsuhiro <kubo@iteman.jp>
2009-04-24 12:05:43 +0900

    Updated.

--- lib/ITEMAN/DynamicPublishing/L10N/ja.pm
+++ lib/ITEMAN/DynamicPublishing/L10N/ja.pm
@@ -25,15 +25,16 @@ our %Lexicon = (
 
 # plugins/ITEMANDynamicPublishing/ITEMANDynamicPublishing.pl
     'ITEMAN Dynamic Publishing allows you to create dynamic Web pages with normal publishing workflow. Since ITEMAN Dynamic Publishing is written in Perl, it can be used with any plug-ins.' => 'ITEMAN Dynamic Publishing は通常のパブリッシングワークフローによる動的な Web ページの作成を可能にします。ITEMAN Dynamic Publishing は Perl で書かれているため、あらゆるプラグインとともに使うことができます。',
-    'Directory Index is required' => 'ディレクトリインデックス は必須です',
-    'Cache Directory is required' => 'キャッシュディレクトリ は必須です',
     'ITEMAN, Inc.' => '株式会社アイテマン',
+    'Directory Index is required' => 'ディレクトリインデックス は必須です',
+    'The error page for the status code 404 is required' => 'ステータスコード 404 のためのエラーページは必須です',
+    'The error page for the status code 500 is required' => 'ステータスコード 500 のためのエラーページは必須です',
 
 # plugins/ITEMANDynamicPublishing/tmpl/system.tmpl
     'Directory Index' => 'ディレクトリインデックス',
     'Directory Index represents the file name which is used if a file name does not included in the requested URI. The default value of the directory index is index.html. You should only change this if you are using a different directory index such as index.htm.' => 'ディレクトリインデックス は、リクエストされた URI にファイル名が含まれない場合に使われるファイル名を表します。ディレクトリインデックス のデフォルト値は index.html です。index.htm のように異なる ディレクトリインデックス を利用する場合に限って、この設定を変更してください。',
     'Cache Directory' => 'キャッシュディレクトリ',
-    'ITEMAN Dynamic Publishing caches of the result of a query to the database for performance. A cache file will be written to the directory specified by Cache Directory. The default value of Cache Directory is tmp under the directory where this plug-in has been deployed. Cache Directory should be readable/writable by the web server.' => 'ITEMAN Dynamic Publishing はパフォーマンス向上のためにデータベースへの問い合わせ結果をキャッシュします。キャッシュファイルは キャッシュディレクトリ によって指定されたディレクトリに書き込まれます。キャッシュディレクトリ のデフォルト値はこのプラグインが配備されているディレクトリの下の tmp です。キャッシュディレクトリ は Web サーバから読み込み・書き込み可能でなければなりません。',
+    'ITEMAN Dynamic Publishing caches of the result of a query to the database for performance. A cache file will be written to the directory specified by Cache Directory. Cache Directory is tmp under the directory where this plug-in has been deployed. Cache Directory should be readable/writable by the web server.' => 'ITEMAN Dynamic Publishing はパフォーマンス向上のためにデータベースへの問い合わせ結果をキャッシュします。キャッシュファイルは キャッシュディレクトリ によって指定されたディレクトリに書き込まれます。キャッシュディレクトリ はこのプラグインが配備されているディレクトリの下の tmp です。キャッシュディレクトリ は Web サーバから読み込み・書き込み可能でなければなりません。',
     'Clear Caches' => 'キャッシュのクリア',
     'Error Pages' => 'エラーページ',
     'Any error page can be specified for 404 and 505 errors. The value should be one of the absolute path of a HTML file, the absolute path of a MT template file, or a URI for redirection.' => '404 および 505 エラー用に任意のエラーページを指定することができます。値は HTML ファイルの絶対パス、MT テンプレートファイルの絶対パス、リダイレクション用 URI のいずれかでなければなりません。'
