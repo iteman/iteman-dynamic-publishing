90141b0105f8eb3060b59cd04d93ddc72199d8ab
KUBO Atsuhiro <kubo@iteman.jp>
2009-05-08 19:26:15 +0900

    Added a license file and updated the GPL notices in all perl scripts.

--- lib/ITEMAN/DynamicPublishing/L10N/ja.pm
+++ lib/ITEMAN/DynamicPublishing/L10N/ja.pm
@@ -1,19 +1,20 @@
+# ITEMAN Dynamic Publishing - A Perl based dynamic publishing extension for Moveble Type
 # Copyright (c) 2009 ITEMAN, Inc. All rights reserved.
 #
 # This file is part of ITEMAN Dynamic Publishing.
 # 
-# ITEMAN Dynamic Publishing is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 # 
-# ITEMAN Dynamic Publishing is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with ITEMAN Dynamic Publishing.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 package ITEMAN::DynamicPublishing::L10N::ja;
 
