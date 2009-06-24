c3c9929810057d055c793d22e1936c9e83d5d55b
KUBO Atsuhiro <kubo@iteman.jp>
2009-06-24 18:13:46 +0900

    Changed the message for the 404 error page.

--- lib/ITEMAN/DynamicPublishing/L10N/ja.pm
+++ lib/ITEMAN/DynamicPublishing/L10N/ja.pm
@@ -44,7 +44,7 @@ our %Lexicon = (
     'Directory Index represents the file name which is used if the requested URI does not contain a file name. The default value of the directory index is index.html. You should only change this if you are using a different directory index such as index.htm.' => 'ディレクトリインデックス は、リクエストされた URI にファイル名が含まれない場合に使われるファイル名を表します。ディレクトリインデックス のデフォルト値は index.html です。index.htm のように異なる ディレクトリインデックス を利用する場合に限って、この設定を変更してください。',
     'ITEMAN Dynamic Publishing caches of the result of a query to the database for performance. A cache file will be written to the directory specified by Cache Directory. Cache Directory is tmp under the directory where this plug-in has been deployed. Cache Directory should be readable/writable by the web server.' => 'ITEMAN Dynamic Publishing はパフォーマンス向上のためにデータベースへの問い合わせ結果をキャッシュします。キャッシュファイルは キャッシュディレクトリ によって指定されたディレクトリに書き込まれます。キャッシュディレクトリ はこのプラグインが配備されているディレクトリの下の tmp です。キャッシュディレクトリ は Web サーバから読み込み・書き込み可能でなければなりません。',
     'Error Page for 404 (Not Found)' => '404 (Not Found) 用エラーページ',
-    'Any error page can be specified for the 404 error. The value should be one of the absolute path of a HTML file, the absolute path of a MT template file, or a URI for redirection.' => '404 エラー用に任意のエラーページを指定することができます。値は HTML ファイルの絶対パス、MT テンプレートファイルの絶対パス、リダイレクション用 URI のいずれかでなければなりません。',
+    'Any error page can be specified for the 404 error. The value should be one of the path of a HTML file, the path of a MT template file, or a URI for redirection.' => '404 エラー用に任意のエラーページを指定することができます。値は HTML ファイルのパス、MT テンプレートファイルのパス、リダイレクション用 URI のいずれかでなければなりません。',
     'The file is not found or not readable for [_1]' => 'ファイルが見つからないか、[_1] ユーザからファイルへの読み込み権限がありません',
     'The file is not readable for [_1]' => '[_1] ユーザからファイルへの読み込み権限がありません',
 );
